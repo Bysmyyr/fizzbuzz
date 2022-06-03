ac0e33522d3bb217fa5856dee81b008e354d847e
Olli Raula <olli.raula@relexsolutions.com>
2022-06-03 18:34:46 +0300

    Threading

--- main.go
+++ main.go
@@ -4,37 +4,56 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
-func main() {
+const routines = 2
 
-	counter := 0
+func main() {
+	eightZeroLock := &sync.Mutex{}
+	startLock := eightZeroLock
+	endLock := &sync.Mutex{}
+	endLock.Lock()
+	for i := 0; i < routines-1; i++ {
+		go routine(i, startLock, endLock)
+		startLock = endLock
+		endLock = &sync.Mutex{}
+		endLock.Lock()
+	}
+	go routine(7, startLock, eightZeroLock)
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	wg.Wait()
+}
+
+func routine(num int, start, end *sync.Mutex) {
+	counter := num * 15
 	for {
 		var sb strings.Builder
 
-		for i := 0; i < 1000; i++ {
-			sb.WriteString(strconv.Itoa((counter + 1)))
-			sb.WriteString("\n")
-			sb.WriteString(strconv.Itoa((counter + 2)))
-			sb.WriteString("\nFizz\n")
-			sb.WriteString(strconv.Itoa((counter + 4)))
-			sb.WriteString("\nBuzz\nFizz\n")
-			sb.WriteString(strconv.Itoa((counter + 7)))
-			sb.WriteString("\n")
-			sb.WriteString(strconv.Itoa((counter + 8)))
-			sb.WriteString("\nFizz\nBuzz\n")
-			sb.WriteString(strconv.Itoa((counter + 11)))
-			sb.WriteString("\nFizz\n")
-			sb.WriteString(strconv.Itoa((counter + 13)))
-			sb.WriteString("\n")
-			sb.WriteString(strconv.Itoa((counter + 14)))
-			sb.WriteString("\nFizzBuzz\n")
-		}
+		sb.WriteString(strconv.Itoa((counter + 1)))
+		sb.WriteString("\n")
+		sb.WriteString(strconv.Itoa((counter + 2)))
+		sb.WriteString("\nFizz\n")
+		sb.WriteString(strconv.Itoa((counter + 4)))
+		sb.WriteString("\nBuzz\nFizz\n")
+		sb.WriteString(strconv.Itoa((counter + 7)))
+		sb.WriteString("\n")
+		sb.WriteString(strconv.Itoa((counter + 8)))
+		sb.WriteString("\nFizz\nBuzz\n")
+		sb.WriteString(strconv.Itoa((counter + 11)))
+		sb.WriteString("\nFizz\n")
+		sb.WriteString(strconv.Itoa((counter + 13)))
+		sb.WriteString("\n")
+		sb.WriteString(strconv.Itoa((counter + 14)))
+		sb.WriteString("\nFizzBuzz\n")
 
+		start.Lock()
 		os.Stdout.Write([]byte(sb.String()))
-		counter += 15
+		end.Unlock()
+		counter += 15 * routines
 	}
-
 }
 
 // fmt.Sprintf
